f5674da0c2e63af050e2036e24cae58fe541b272
amkamir82 <amk_amir82@yahoo.com>
2024-01-05 17:06:30 +0330

    add task generator using Uunifast algorithm

---
+++ task_gen.py
@@ -0,0 +1,20 @@
+import random
+
+
+def UUniFastDiscard(n, u, nsets):
+    sets = []
+    while len(sets) < nsets:
+        # Classic UUniFast algorithm:
+        utilizations = []
+        sumU = u
+        for i in range(1, n):
+            nextSumU = sumU * random.random() ** (1.0 / (n - i))
+            utilizations.append(sumU - nextSumU)
+            sumU = nextSumU
+        utilizations.append(sumU)
+
+        # If no task utilization exceeds 1:
+        if all(ut <= 1 for ut in utilizations):
+            sets.append(utilizations)
+
+    return sets
