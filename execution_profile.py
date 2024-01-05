395e6b83ad0fab8019456487dc3c32e5910713a0
Sadegh Majidi <m.majidi@sharif.edu>
2024-01-05 19:55:58 +0330

    add exec_time to exec profile

--- execution_profile.py
+++ execution_profile.py
@@ -1,9 +1,10 @@
 class ExecutionProfile:
     def __init__(self, average: float, min_val: float, max_val: float, power: float, energy: float,
-                 energy_in_window: float) -> None:
+                 energy_in_window: float, exec_time: float) -> None:
         self.average = average
         self.min = min_val
         self.max = max_val
+        self.exec_time = exec_time
         self.power = power
         self.energy = energy
         self.energy_in_window = energy_in_window
