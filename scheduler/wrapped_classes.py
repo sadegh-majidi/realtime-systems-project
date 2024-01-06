fcc4d6859bcf1cd976db64d33cc97cd8f1b697a2
amkamir82 <amk_amir82@yahoo.com>
2024-01-06 23:18:00 +0330

    merge wrapper classes to a single file

---
+++ scheduler/wrapped_classes.py
@@ -0,0 +1,22 @@
+class FCFSTaskWrapper:
+    def __init__(self, task):
+        self.task = task
+
+    def __getattr__(self, attr):
+        return getattr(self.task, attr)
+
+    def __lt__(self, other):
+        if self.task.arrival == other.task.arrival:
+            return self.task.index < other.task.index
+        return self.task.arrival < other.task.arrival
+
+
+class sBEETTaskWrapper:
+    def __init__(self, task):
+        self.task = task
+
+    def __lt__(self, other):
+        if self.task.arrival == other.task.arrival:
+            return self.task.period < other.task.period
+        else:
+            return self.task.arrival < other.task.arrival
