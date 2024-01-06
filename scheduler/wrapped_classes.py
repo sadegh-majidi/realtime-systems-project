2b3c106f1de1e5aaa707760d32c3f6c55b59d6cd
amkamir82 <amk_amir82@yahoo.com>
2024-01-07 00:05:22 +0330

    set attr of sBEETTaskWrapper

--- scheduler/wrapped_classes.py
+++ scheduler/wrapped_classes.py
@@ -15,6 +15,15 @@ class sBEETTaskWrapper:
     def __init__(self, task):
         self.task = task
 
+    def __getattr__(self, attr):
+        return getattr(self.task, attr)
+
+    def __setattr__(self, name, value):
+        if name == 'task':
+            super().__setattr__(name, value)
+        else:
+            setattr(self.task, name, value)
+
     def __lt__(self, other):
         if self.task.arrival == other.task.arrival:
             return self.task.period < other.task.period
