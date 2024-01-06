ba571baa37fc3f09c141a5ba843bf7244a4a9232
amkamir82 <amk_amir82@yahoo.com>
2024-01-07 00:04:42 +0330

    init SBEET wrapper tasks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from task_gen import TaskGenerator
 from scheduler.wrapped_classes import FCFSTaskWrapper, sBEETTaskWrapper
 from scheduler.scheduler import Scheduler
 from scheduler.FCFS_controller import FSFCController
+from scheduler.sBEET_controller import sBEETController
 
 
 def wrap_tasks(schedule_wrapper, tasks):
@@ -34,6 +35,10 @@ if __name__ == '__main__':
     sbeet_tasks = wrap_tasks_for_sBEET_schedule(tasks)
 
     hyper_period = calculate_hyper_period(fcfs_tasks)
-    fsfc_controller = FSFCController(fcfs_tasks).init_task_queue()
-    scheduler = Scheduler(hyper_period=hyper_period, controller=fsfc_controller)
-    scheduler.run_FCFS_schedule()
+    # fsfc_controller = FSFCController(fcfs_tasks).init_task_queue()
+    # scheduler = Scheduler(hyper_period=hyper_period, controller=fsfc_controller)
+    # scheduler.run_FCFS_schedule()
+
+    sbeet_controller = sBEETController(sbeet_tasks).init_task_queue()
+    scheduler = Scheduler(hyper_period=hyper_period, controller=sbeet_controller)
+    scheduler.run_sBEET_schedule()
