e78c0a0f777ecb1a3356ce5c26a64c2e49aa185e
amkamir82 <amk_amir82@yahoo.com>
2024-01-06 23:18:13 +0330

    fix FCFS schedule algorithm

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -3,8 +3,7 @@ import heapq
 
 class Scheduler:
 
-    def __init__(self, tasks, hyper_period, controller):
-        self.tasks = tasks
+    def __init__(self, hyper_period, controller):
         self.hyper_period = hyper_period
         self.controller = controller
 
@@ -17,8 +16,41 @@ class Scheduler:
         while current_time <= self.hyper_period:
             active_task = self.controller.get_task_to_execute()
             print(f"Executing task #{active_task.name}")
-            current_time += active_task.executation_time
+            current_time += active_task.execution_time
             print(f"Task #{active_task.name} executed")
+            active_task.task.arrival += active_task.period
+            self.controller.add_task_to_queue(active_task)
 
     def run_sBEET_schedule(self):
-        pass
+        current_time = 0
+        remaining_cores = 6
+        while current_time <= self.hyper_period:
+            first_task, second_task = self.controller.get_task_to_execute(remaining_cores)
+
+            remaining_cores -= first_task.cores + second_task.cores
+
+            print(f"Executing task #{first_task.name} on #{first_task.cores} cores")
+            print(f"Executing task #{second_task.name} on #{second_task.cores} cores")
+
+            execution_time = min(first_task.execution_time, second_task.execution_time)
+            current_time += execution_time
+
+            first_task.execution_time -= execution_time
+            second_task.execution_time -= execution_time
+
+            if first_task.execution_time == 0:
+                print(f"Task #{first_task.name} executed")
+                remaining_cores += first_task.cores
+                first_task.arrival_time += first_task.period
+                first_task.cores = None
+                first_task.execution_time = None
+
+            if second_task.execution_time == 0:
+                print(f"Task #{second_task.name} executed")
+                remaining_cores += second_task.cores
+                second_task.arrival_time += second_task.period
+                second_task.cores = None
+                second_task.execution_time = None
+
+            self.controller.add_task_to_queue(first_task)
+            self.controller.add_task_to_queue(second_task)
